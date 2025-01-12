9f2555ed7420e602280b4588476f7856234e10c3
Waliyel Hasnat Zaman <waliyelhasnatzaman@gmail.com>
2025-01-12 21:01:10 +0600

    Update help_support.php

--- help_support.php
+++ help_support.php
@@ -64,7 +64,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $mail->Host = 'smtp.gmail.com';
             $mail->SMTPAuth = true;
             $mail->Username = 'waliyelhasnatzaman@gmail.com';  // Your Gmail email address
-            $mail->Password = 'dswgwrjpxzzgtetk';  // Your Gmail app password
+            $mail->Password = '#';  // Your Gmail app password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port = 587;
 
